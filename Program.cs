a389f59e9b0ef15fa3a1a7e4e089dce50e2a8272
Dheeraj kumar <dheerajkumar14.a@gmail.com>
2024-09-26 19:21:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number to check for Palindrome:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int reverse = 0;
+            int temp = number;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number /= 10;
+            }
+
+            if (temp == reverse)
+                Console.WriteLine("Palindrome");
+            else
+                Console.WriteLine("Not a Palindrome");
+        }
+    }
+}
